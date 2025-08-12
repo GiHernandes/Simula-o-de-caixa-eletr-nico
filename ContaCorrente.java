090573504aea1b22d732dfb9b7264f8ad44c3cac
GiHernandes <giovannahernandes96@gmail.com>
2025-08-11 22:41:17 -0300

    Adiciona versão com juros e limite especial

--- ContaCorrente.java
+++ ContaCorrente.java
@@ -1,4 +1,5 @@
 import java.util.Scanner;
+//import java.time.localDateTime;    
 
 public class ContaCorrente{
     public static void main(String[] args){
@@ -7,14 +8,20 @@ public class ContaCorrente{
         String nome = "Giovanna";
         String tipoConta = "Conta corrente";
         Double saldo = 1800.00;
+        Double limiteEspecial = 500.00; // até onde pode ficar negativo
+        Double jurosPositivo = 0.1; // 1% ao mês
+        Double jurosNegativos = 0.5; // 5% ao mês
+        Double simulacao = 0.0;
         int escolha = 0;
+        String ultimaOperacao = "Nenhuma";
+        double valorUltimaOperacao = 0.0;
 
-        while (escolha != 4){
+        while (escolha != 6){
         // Apresentar dados iniciais...
         System.out.println("**********************************************************");
         System.out.println("\nOlá " + nome + ", seja bem-vindo(a)!");
         System.out.println("\nConta: " + tipoConta);
-        System.out.println("\nSaldo atual: " + saldo);
+        System.out.printf("\nSaldo atual: R$ %.2f%n", saldo);
         System.out.println("\n**********************************************************");
 
         //Prompt de opções:
@@ -22,44 +29,78 @@ public class ContaCorrente{
         System.out.println("\n[1] Sacar");
         System.out.println("[2] Depositar");
         System.out.println("[3] Transferir");
-        System.out.println("[4] Sair");
+        System.out.println("[4] Última operação");
+        System.out.println("[5] Simular passagem de mês");
+        System.out.println("[6] Sair");
         escolha = leitura.nextInt();
-        
-            if(escolha == 1){
+            
+            switch (escolha) {
+                case 1:
                 System.out.println("Digite o valor a ser sacado: ");
                 Double saque = leitura.nextDouble();
-                if (saque <= saldo){
+                if (saque > 0 && saque <= saldo){
                     saldo -= saque;
+                    ultimaOperacao = "saque";
+                    valorUltimaOperacao = saque;
                     System.out.println("Saque realizado com sucesso," + nome + "!");
                     System.out.println("Saldo atual: %.2f" + saldo);
                 } else{
                     System.out.println("Saldo insuficiente para realizar o saque desejado...");
                 }
-            }
+                break;
 
-            else if(escolha == 2){
+            case 2:
                 System.out.println("Digite o valor a ser depositado: ");
                 Double deposito = leitura.nextDouble();
-                saldo += deposito;
-                System.out.println("Depósito realizado com sucesso, " + nome + "!. \nSeu novo saldo é: " + saldo);
-            }
+                if (deposito > 0){
+                    saldo += deposito;
+                    ultimaOperacao = "Depósito";
+                    valorUltimaOperacao = deposito;
+                    System.out.println("Depósito realizado com sucesso, " + nome + "!. \nSeu novo saldo é: " + saldo);
+                } else {
+                    System.out.println("Valor menor ou igual a zero. A operação não será realizada...");
+                }
+                break;
             
-            else if(escolha == 3){
+                case 3:
                 System.out.println("Digite o valor a ser transferido: ");
                 Double transferencia = leitura.nextDouble();
-                if(transferencia <= saldo){
+                if(transferencia > 0 && transferencia <= saldo){
                     saldo -= transferencia;
+                    ultimaOperacao = "Transferência";
+                    valorUltimaOperacao = transferencia;
                     System.out.println("Transferência realizada com sucesso, " + nome + "! \nSeu novo saldo é: " + saldo);
                 } else{
                     System.out.println("Saldo insuficiente para realizar a transferência desejada...");
                 }
-            
-            }
-            else if(escolha == 4){
-                System.out.println("Obrigada por utilizar nossos serviços " + nome + "!\nVolte sempre!!");
                 break;
-            } else{
-                System.out.println("Opção inválida. \nTente novamente.");
+
+                case 4:
+                    System.out.printf("Última operação: %s de R$ %.2f%n", ultimaOperacao, valorUltimaOperacao);
+                    break;
+                
+                case 5:
+                    System.out.println("Quantos meses deseja simular?");
+                    int meses = leitura.nextInt();
+                    if (meses > 0){
+                        for (int i = 1; i <= meses; i++){
+                            if (saldo >= 0){
+                                simulacao = saldo + (saldo * jurosPositivo);
+                            } else {
+                                simulacao = saldo + (saldo * jurosNegativos);
+                            }
+                        }
+                        System.out.printf("Após %d meses, seu saldo é R$ %.2f%n", meses, simulacao);
+                    } else {
+                        System.out.println("Quantidade inválida.");
+                    }
+                    break;
+            
+                case 6:
+                    System.out.println("Obrigada por utilizar nossos serviços " + nome + "!\nVolte sempre!!");
+                    break;
+                default:
+                    System.out.println("Opção inválida. \nTente novamente.");
             } 
         }       
     }
